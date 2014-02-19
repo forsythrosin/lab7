77b1162cc406884d4f259864e0e9d569eceeebf9
Tomas Forsyth Rosin <tomasrosin@gmail.com>
2014-02-19 22:01:29 +0000

    storing data lab

--- routes/project.js
+++ routes/project.js
@@ -3,8 +3,9 @@ var models = require('../models');
 exports.projectInfo = function(req, res) { 
   var projectID = req.params.id;
 
-  // query for the specific project and
-  // call the following callback
+  models.Project
+    .find({'_id':projectID})
+    .exec(afterQuery);
 
   function afterQuery(err, projects) {
     if(err) console.log(err);
@@ -14,15 +15,32 @@ exports.projectInfo = function(req, res) { 
 
 exports.addProject = function(req, res) {
   var form_data = req.body;
-  console.log(form_data);
 
-  // make a new Project and save it to the DB
-  // YOU MUST send an OK response w/ res.send();
+  var newProject = new models.Project({
+    'title': form_data.project_title,
+    'date': form_data.date,
+    'summary': form_data.summary,
+    'image': form_data.image_url
+  });
+
+  newProject.save(afterSave);
+
+  function afterSave(err) {
+    if(err) console.log(err);
+    res.send();
+  }
 }
 
 exports.deleteProject = function(req, res) {
   var projectID = req.params.id;
 
-  // find the project and remove it
-  // YOU MUST send an OK response w/ res.send();
+  models.Project
+    .find({'_id':projectID})
+    .remove()
+    .exec(afterDeleted);
+
+  function afterDeleted(err) {
+    if(err) console.log(err);
+    res.send();
+  }
 }
